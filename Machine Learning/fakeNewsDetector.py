51ed280e55113b86d84659f84164ac66e56fbe5c
Rohit Shaw <carpediem212045@gmail.com>
2022-10-14 20:23:19 +0530

    Created a fake news detector using python
    This'll help you to detect the anonymous/fake news in today's creepy world scenario!

--- Machine Learning/fakeNewsDetector.py
+++ Machine Learning/fakeNewsDetector.py
@@ -17,7 +17,6 @@ x_train, x_test, y_train, y_test = train_test_split(
     df['text'], labels, test_size=0.2, random_state=7)
 
 tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
-
 tfidf_train = tfidf_vectorizer.fit_transform(x_train)
 tfidf_test = tfidf_vectorizer.transform(x_test)
 
